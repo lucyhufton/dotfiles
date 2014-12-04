661ac44619e66861ebf78643bdaad8066ec54f0b
Daniel Furze <daniel@furzeface.com>
2014-12-04 18:56:52 +0000

    adds cask script

---
+++ .cask.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Core Functionality
+echo Installing Core Apps
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" google-drive
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" vlc
+
+# Development
+echo Installing Dev apps
+brew cask install --appdir="/Applications" adobe-creative-cloud
+brew cask install --appdir="/Applications" sublime-text
+
+# brew cask install --appdir="/Applications" imagealpha
+# brew cask install --appdir="/Applications" imageoptim
+
+# Browsers
+echo Installing browsers
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" firefox
+# TODO: Canary and Nightly.
+
+# Additional apps
+echo Installing some additional apps
+brew cask install --appdir="/Applications" appcleaner
+brew cask install --appdir="/Applications" spotify
+
+# Linking Cask apps to Alfred
+echo Linking apps to Alfred
+brew cask alfred link
+
+# cleanup
+echo Cleaning up
+brew cleanup --force
+
+rm -f -r /Library/Caches/Homebrew/*

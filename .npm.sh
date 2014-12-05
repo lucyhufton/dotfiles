d7600f170f226eb707d110be9856a908d4ddb125
Daniel Furze <daniel@furzeface.com>
2014-12-05 08:17:39 +0000

    adds npm adduser

--- .npm.sh
+++ .npm.sh
@@ -6,6 +6,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+npm adduser furzeface
+
 npm i bower -g
 npm i grunt grunt-cli -g
 npm i yo -g

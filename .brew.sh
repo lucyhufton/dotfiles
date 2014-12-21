23455e0978b29983d57ce43a059489cdf8e6f0e3
Daniel Furze <daniel@furzeface.com>
2014-12-21 21:41:25 +0000

    adds utilities

--- .brew.sh
+++ .brew.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools using Homebrew
 
-# Ask for the administrator password upfront.
+# Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install Homebrew
-echo Installing Homebrew \(gets Xcode cli developer tools too\).
+echo Installing Homebrew \(gets Xcode cli developer tools too\)…
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install some utilities
+brew install wget --with-iri
+brew install lynx
+
 # Install Git
-echo Installing Git
+echo Installing Git…
 brew install git
 brew install git-extras
 
-# Install Node.js. Note: this installs `npm` too, using the recommended installation method.
-echo Installing Node
+# Install Node.js - this installs `npm` too, using the recommended installation method
+echo Installing Node…
 brew install node
 
-# Install Brew Cask for managing other apps.
-echo Installing Brew Cask
+# Install Brew Cask for managing other apps
+echo Installing Brew Cask…
 brew install caskroom/cask/brew-cask
+
 # Install Cask versions for beta version of apps :D
 brew tap caskroom/versions

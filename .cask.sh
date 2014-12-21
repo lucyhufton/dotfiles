fa10d61170ffc4a09772724fccdd2f02c43c37e2
Daniel Furze <daniel@furzeface.com>
2014-12-21 21:41:07 +0000

    adds new text editors and browsers

--- .cask.sh
+++ .cask.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Core Functionality
-echo Installing core apps
+echo Installing core apps…
 brew cask install --appdir="/Applications" alfred
 brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="/Applications" google-drive
@@ -11,8 +11,10 @@ brew cask install --appdir="/Applications" time-out
 brew cask install --appdir="/Applications" vlc
 
 # Development apps
-echo Installing dev apps
+echo Installing dev apps…
 brew cask install --appdir="/Applications" adobe-creative-cloud
+brew cask install --appdir="/Applications" atom
+brew cask install --appdir="/Applications" brackets
 brew cask install --appdir="/Applications" filezilla
 brew cask install --appdir="/Applications" imagealpha
 brew cask install --appdir="/Applications" imageoptim
@@ -23,25 +25,26 @@ brew cask install --appdir="/Applications" vagrant
 brew cask install --appdir="/Applications" virtualbox
 
 # Browsers
-echo Installing browsers
+echo Installing browsers…
 brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" firefox-nightly
 brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" google-chrome-canary
+brew cask install --appdir="/Applications" opera
 
 # Additional apps
-echo Installing some additional apps
+echo Installing some additional apps…
 brew cask install --appdir="/Applications" appcleaner
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" tunnelbear
 brew cask install --appdir="/Applications" utorrent
 
 # Linking Cask apps to Alfred
-echo Linking apps to Alfred
+echo Linking apps to Alfred…
 brew cask alfred link
 
 # cleanup
-echo Cleaning up
+echo Cleaning up…
 brew cleanup --force
 
 rm -f -r /Library/Caches/Homebrew/*

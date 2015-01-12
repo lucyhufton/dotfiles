1c31e0fc8e7ded354cc5136537252c5c73033eee
Daniel Furze <daniel@furzeface.com>
2015-01-12 10:07:11 +0000

    comments npm adduser for if you're not me

--- .npm.sh
+++ .npm.sh
@@ -3,10 +3,10 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+# Keep-alive: update existing `sudo` time stamp until `.npm.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-npm adduser furzeface
+# npm adduser furzeface # uncomment if you’re me
 
 npm i bower -g
 npm i grunt grunt-cli -g

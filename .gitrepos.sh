c69928031e12960af1cad41ec5aad439b9863334
Daniel Furze <daniel@furzeface.com>
2015-01-13 08:16:33 +0000

    clone ALL my GitHub repos automagically :tada:

--- .gitrepos.sh
+++ .gitrepos.sh
@@ -3,11 +3,12 @@
 # iTerm colour schemes {iterm2colorschemes.com}
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/iTerm2-Color-Schemes
 
-# Sublime JS snippets
+# Sublime JS snippets > into Sublime packages directory
 git clone https://github.com/furzeface/JS-Snippets ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/JS
 
-# Sublime CSS snippets {http://joshnh.com/2012/02/03/a-collection-of-css-snippets-for-sublime-text-2}
+# Sublime CSS snippets > into Sublime packages directory
+# {http://joshnh.com/2012/02/03/a-collection-of-css-snippets-for-sublime-text-2}
 git clone https://github.com/joshnh/CSS-Snippets ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/CSS
 
 # My repos
-git clone https://github.com/furzeface/daniel.furzeface.com ~/Sites/GitHub/daniel.furzeface.com
+curl -s "https://api.github.com/users/furzeface/repos" | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each {|repo| %x[git clone #{repo["ssh_url"]} ]}'

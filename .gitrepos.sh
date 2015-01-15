847c6e336da584340314bab01fa476e1dedac5cc
Daniel Furze <daniel@furzeface.com>
2015-01-15 09:48:30 +0000

    amends repo directory destination

--- .gitrepos.sh
+++ .gitrepos.sh
@@ -1,6 +1,7 @@
 # Clone git repos
 
-# iTerm colour schemes {iterm2colorschemes.com}
+# iTerm colour schemes 
+# {iterm2colorschemes.com}
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/iTerm2-Color-Schemes
 
 # Sublime JS snippets > into Sublime packages directory
@@ -10,5 +11,6 @@ git clone https://github.com/furzeface/JS-Snippets ~/Library/Application\ Suppor
 # {http://joshnh.com/2012/02/03/a-collection-of-css-snippets-for-sublime-text-2}
 git clone https://github.com/joshnh/CSS-Snippets ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/CSS
 
-# My repos
+# My repos - change username to yours if you want
+cd ~/Sites/GitHub # Change to your repos directory
 curl -s "https://api.github.com/users/furzeface/repos" | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each {|repo| %x[git clone #{repo["ssh_url"]} ]}'

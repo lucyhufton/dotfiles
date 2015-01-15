f90c5f697659b41c87f73b765afcb641a367ff13
Daniel Furze <daniel@furzeface.com>
2015-01-15 14:31:04 +0000

    adds colors to script

--- .update-repos.sh
+++ .update-repos.sh
@@ -5,28 +5,28 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Go to GitHub repos
-cd ~/Sites/GitHub;
+# cd ~/Sites/GitHub;
 
-for n in *;
-	do echo "Updating" $n"…";
+# for n in *
+# 	do echo "`tput setaf 4`Updating" $n"…`tput setaf 7`"
 
-	cd "$n" && git pull && cd ../;
-done
+# 	cd "$n" && git pull && cd ../
+# done
 
-echo -e "Done…All GitHub repos are up to date.\n";
+# echo -e "`tput setaf 2`Done…All GitHub repos are up to date.\n `tput setaf 7`"
 
-unset n;
+# unset n
 
 
 # Go to BB repos
-cd ~/Sites/BuildingBlocks;
+cd ~/Sites/BuildingBlocks
 
-for n in *;
-	do echo "Updating" $n"…";
+for n in *
+	do echo "`tput setaf 4`Updating" $n"…`tput setaf 7`"
 
-	cd "$n" && git pull && cd ../;
+	cd "$n" && git pull && cd ../
 done
 
-echo -e "Done…All BB repos are up to date.\n";
+echo -e "`tput setaf 2`Done…All BB repos are up to date.\n `tput setaf 7`"
 
-unset n;
+unset n

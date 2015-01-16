99e7f72b6b2a7607bc71c99c1dceac6b6163b277
Daniel Furze <daniel@furzeface.com>
2015-01-16 12:51:48 +0000

    bail out if not directory, resolves #3

--- .update-repos.sh
+++ .update-repos.sh
@@ -8,15 +8,15 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 cd ~/Sites/GitHub;
 
 for n in *; do
-	cd "$n"
-
-		if [ ! -d ".git" ]; then
-			echo "`tput setaf 1`'$n' is not a git repository…`tput setaf 7`";
-		else
-			echo "`tput setaf 4`Updating" $n"…`tput setaf 7`" && git pull;
-		fi
-
-	cd ../
+	if [ -d "$n" ]; then
+		cd "$n"
+			if [ ! -d ".git" ]; then
+				echo "`tput setaf 1`'$n' is not a git repository…`tput setaf 7`"
+			else
+				echo "`tput setaf 4`Updating" $n"…`tput setaf 7`" && git pull
+			fi
+		cd ../
+	fi
 done
 
 echo -e "`tput setaf 2`Done…All GitHub repos are up to date.\n `tput setaf 7`"
@@ -27,17 +27,16 @@ unset n
 # Go to BB repos
 cd ~/Sites/BuildingBlocks
 
-# @todo Bail out if it’s not a directory.
 for n in *; do
-	cd "$n"
-
-		if [ ! -d ".git" ]; then
-			echo "`tput setaf 1`'$n' is not a git repository…`tput setaf 7`";
-		else
-			echo "`tput setaf 4`Updating" $n"…`tput setaf 7`" && git pull;
-		fi
-
-	cd ../
+	if [ -d "$n" ]; then
+		cd "$n"
+			if [ ! -d ".git" ]; then
+				echo "`tput setaf 1`'$n' is not a git repository…`tput setaf 7`"
+			else
+				echo "`tput setaf 4`Updating" $n"…`tput setaf 7`" && git pull
+			fi
+		cd ../
+	fi
 done
 
 echo -e "`tput setaf 2`Done…All BB repos are up to date.\n `tput setaf 7`"

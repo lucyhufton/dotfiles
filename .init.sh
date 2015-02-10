36464a8501f1a83cc61a1a68716358c1366afd5a
Daniel Furze <daniel@furzeface.com>
2015-02-10 11:54:58 +0000

    adds sublime settings and script to copy into correct location on .init

--- .init.sh
+++ .init.sh
@@ -3,5 +3,11 @@
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
+# Copy Sublime settings files
+cp init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
+# Copy Sublime Text key bindings
+cp init/Default\ (OSX).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+
 # Install vagrant plugins
 vagrant plugin install vagrant-hostsupdater

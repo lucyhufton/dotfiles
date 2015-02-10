b2362673af57ed45f261bf17be9cb971de928d5a
Daniel Furze <daniel@furzeface.com>
2015-02-10 11:50:10 +0000

    clone repos into ST3

--- .gitrepos.sh
+++ .gitrepos.sh
@@ -1,15 +1,15 @@
 # Clone git repos
 
-# iTerm colour schemes 
+# iTerm colour schemes
 # {iterm2colorschemes.com}
 git clone https://github.com/mbadolato/iTerm2-Color-Schemes ~/iTerm2-Color-Schemes
 
 # Sublime JS snippets > into Sublime packages directory
-git clone https://github.com/furzeface/JS-Snippets ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/JS
+git clone https://github.com/furzeface/JS-Snippets ~/Library/Application\ Support/Sublime\ Text/Packages/User/JS
 
 # Sublime CSS snippets > into Sublime packages directory
 # {http://joshnh.com/2012/02/03/a-collection-of-css-snippets-for-sublime-text-2}
-git clone https://github.com/joshnh/CSS-Snippets ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/CSS
+git clone https://github.com/joshnh/CSS-Snippets ~/Library/Application\ Support/Sublime\ Text/Packages/User/CSS
 
 # My repos - change username to yours if you want
 cd ~/Sites/GitHub # Change to your repos directory

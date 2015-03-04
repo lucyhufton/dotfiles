a3f05d6f37e45145d8707dd064aeeb261820304b
Daniel Furze <daniel@furzeface.com>
2015-03-04 11:12:47 +0000

    adds Sublime package manager config - tip of the hat to @PJL101 :sunglasses:

--- .init.sh
+++ .init.sh
@@ -3,9 +3,10 @@
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-# Copy Sublime settings files
+# Copy Sublime settings file
 cp init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-
+# Copy Sublime package settings file
+cp init/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 # Copy Sublime Text key bindings
 cp init/Default\ (OSX).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 

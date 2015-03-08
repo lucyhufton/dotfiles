382bf2effb762be36e8c0cdafb44f51f1b6c3c7f
Daniel Furze <daniel@furzeface.com>
2015-03-08 12:06:53 +0000

    tidies up cask

--- .cask.sh
+++ .cask.sh
@@ -4,30 +4,12 @@
 
 # Core Functionality
 echo Installing core apps…
-brew cask install --appdir="/Applications" alfred
 brew cask install --appdir="/Applications" dropbox
 brew cask install --appdir="/Applications" google-drive
 brew cask install --appdir="/Applications" iterm2
 brew cask install --appdir="/Applications" spectacle
 brew cask install --appdir="/Applications" time-out
 brew cask install --appdir="/Applications" vlc
-
-# Development apps
-echo Installing dev apps…
-# brew cask install --appdir="/Applications" adobe-creative-cloud
-# brew cask install --appdir="/Applications" atom
-# brew cask install --appdir="/Applications" brackets
-brew cask install --appdir="/Applications" color-oracle
-brew cask install --appdir="/Applications" filezilla
-brew cask install --appdir="/Applications" imagealpha
-brew cask install --appdir="/Applications" imageoptim
-brew cask install --appdir="/Applications" mamp # Cos you’ll need PHP SOME time…
-brew cask install --appdir="/Applications" remote-desktop-connection
-brew cask install --appdir="/Applications" sublime-text3
-brew cask install --appdir="/Applications" sourcetree # Cos you’ll need a GUI SOME time…
-brew cask install --appdir="/Applications" textwrangler
-brew cask install --appdir="/Applications" vagrant
-brew cask install --appdir="/Applications" virtualbox
 
 # Browsers
 echo Installing browsers…
@@ -37,17 +19,32 @@ brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" google-chrome-canary
 brew cask install --appdir="/Applications" opera
 
+# Development apps
+echo Installing dev apps…
+brew cask install --appdir="/Applications" adobe-creative-cloud
+brew cask install --appdir="/Applications" android-studio
+brew cask install --appdir="/Applications" atom
+brew cask install --appdir="/Applications" brackets
+brew cask install --appdir="/Applications" color-oracle
+# brew cask install --appdir="/Applications" cord # Uncomment if you want this instead of RCD
+brew cask install --appdir="/Applications" filezilla
+brew cask install --appdir="/Applications" imagealpha
+brew cask install --appdir="/Applications" imageoptim
+# brew cask install --appdir="/Applications" mamp # Cos you’ll need it SOME time…
+brew cask install --appdir="/Applications" remote-desktop-connection # Comment this out if you want CoRD instead
+brew cask install --appdir="/Applications" sublime-text3
+# brew cask install --appdir="/Applications" sourcetree # Cos you’ll need a GUI SOME time…
+brew cask install --appdir="/Applications" textwrangler
+brew cask install --appdir="/Applications" vagrant
+brew cask install --appdir="/Applications" virtualbox
+
 # Additional apps
 echo Installing some additional apps…
 brew cask install --appdir="/Applications" appcleaner
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" tunnelbear
-# brew cask install --appdir="/Applications" utorrent
+brew cask install --appdir="/Applications" utorrent
 
-
-# Linking Cask apps to Alfred
-echo Linking apps to Alfred…
-brew cask alfred link
 
 # cleanup
 echo Cleaning up…

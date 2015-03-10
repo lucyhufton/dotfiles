f50a09701bfc8b81136e7353ad895e2768165f7c
Dan <daniel@furzeface.com>
2015-03-10 21:32:59 +0000

    Update .update-repos.sh

--- .update-repos.sh
+++ .update-repos.sh
@@ -22,23 +22,3 @@ done
 echo -e "`tput setaf 2`Done…All GitHub repos are up to date.\n `tput setaf 7`"
 
 unset n
-
-
-# Go to BB repos
-cd ~/Sites/BuildingBlocks
-
-for n in *; do
-	if [ -d "$n" ]; then
-		cd "$n"
-			if [ ! -d ".git" ]; then
-				echo "`tput setaf 1`'$n' is not a git repository…`tput setaf 7`"
-			else
-				echo "`tput setaf 4`Updating" $n"…`tput setaf 7`" && git pull
-			fi
-		cd ../
-	fi
-done
-
-echo -e "`tput setaf 2`Done…All BB repos are up to date.\n `tput setaf 7`"
-
-unset n

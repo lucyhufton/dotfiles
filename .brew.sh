8cd61cbbeb66f42e5c7b46c6e5fc4d2912c3cdc9
lucyhufton <lmh@amp.co>
2015-09-15 13:17:00 +0100

    Update .brew.sh

--- .brew.sh
+++ .brew.sh
@@ -10,6 +10,27 @@ sudo xcodebuild -license
 # Install some utilities
 brew install wget --with-iri
 brew install lynx
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
 
 # Install Git
 echo Installing Git…

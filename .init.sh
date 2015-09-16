cd908bfa806fedf9cd8ce9af44ad3e7ff3efdd76
lucyhufton <lmh@amp.co>
2015-09-16 11:25:03 +0100

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -3,12 +3,5 @@
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
-# Copy Sublime settings file
-cp init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-# Copy Sublime package settings file
-cp init/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-# Copy Sublime Text key bindings
-cp init/Default\ (OSX).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-
 # Install vagrant plugins
 vagrant plugin install vagrant-hostsupdater

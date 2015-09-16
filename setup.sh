36d563837f500d3636fb50d7b9ef0d5ec80ee61a
lucyhufton <lmh@amp.co>
2015-09-16 13:44:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,3 +13,12 @@ hdiutil unmount puppet-agent-1.2.4-1.yosemite.dmg
 rm -f /Users/admin/downloads/puppet-agent-1.2.4-1.yosemite.dmg
 
 mv /Users/admin/Downloads/dotfile.master/* ~/
+
+dscl . -create /Users/admin
+dscl . -create /Users/admin UserShell /bin/bash
+dscl . -create /Users/admin RealName "admin"
+dscl . -create /Users/admin UniqueID "1010"
+dscl . -create /Users/admin PrimaryGroupID 80
+dscl . -create /Users/admin NFSHomeDirectory /Users/admin
+dscl . -passwd /Users/luser lc1208
+dscl . -append /Groups/admin GroupMembership admin

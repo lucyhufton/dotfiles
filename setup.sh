55f2a48f2b64cacbb4b963743adf436e2a269565
lucyhufton <lmh@amp.co>
2015-09-16 15:35:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,21 +6,4 @@ bash .init.sh
 bash .mkdir.sh
 bash .osx.sh
 
-wget http://downloads.puppetlabs.com/mac/PC1/puppet-agent-1.2.4-1.yosemite.dmg
-hdiutil mount /Users/admin/puppet-agent-1.2.4-1.yosemite.dmg
-sudo installer -pkg /Volumes/puppet-agent-1.2.4-1.yosemite/puppet-agent-1.2.4-1.yosemite.pkg -target /
-hdiutil unmount puppet-agent-1.2.4-1.yosemite.dmg
-rm -f /Users/admin/downloads/puppet-agent-1.2.4-1.yosemite.dmg
-
-mv /Users/admin/Downloads/dotfile-master/* ~/
-
-dscl . -create /Users/admin
-dscl . -create /Users/admin UserShell /bin/bash
-dscl . -create /Users/admin RealName "admin"
-dscl . -create /Users/admin UniqueID "1010"
-dscl . -create /Users/admin PrimaryGroupID 80
-dscl . -create /Users/admin NFSHomeDirectory /Users/admin
-dscl . -passwd /Users/admin lc1208
-dscl . -append /Groups/admin GroupMembership admin
-
-rm -f .brew.sh .cask.sh .init.sh .mkdir.sh .osx.sh .setup.sh
+rm -f .brew.sh .cask.sh .init.sh .mkdir.sh .osx.sh setup.sh

84d112461da0582470ca4529450529c46a753336
lucyhufton <lmh@amp.co>
2015-09-17 11:36:20 +0100

    Update .cask.sh

--- .cask.sh
+++ .cask.sh
@@ -6,13 +6,8 @@
 brew cask install --appdir="/Applications" spectacle
 
 # Browsers
-brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" google-chrome
 
-# brew cask install --appdir="/Applications" cord # Uncomment if you want this instead of RCD
-brew cask install --appdir="/Applications" imageoptim
-# brew cask install --appdir="/Applications" mamp # Cos you’ll need it SOME time…
-brew cask install --appdir="/Applications" remote-desktop-connection # Comment this out if you want CoRD instead
 # brew cask install --appdir="/Applications" sourcetree # Cos you’ll need a GUI SOME time…
 brew cask install --appdir="/Applications" textwrangler
 brew cask install --appdir="/Applications" vagrant

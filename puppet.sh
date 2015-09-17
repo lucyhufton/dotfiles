9a80379aabde6c0882b04b01b1e71f43d102bf82
lucyhufton <lmh@amp.co>
2015-09-17 15:00:40 +0100

    Update puppet.sh

--- puppet.sh
+++ puppet.sh
@@ -2,7 +2,8 @@
 
 wget http://downloads.puppetlabs.com/mac/PC1/puppet-agent-1.2.4-1.yosemite.dmg
 sudo hdiutil mount puppet-agent-1.2.4-1.yosemite.dmg
-sudo installer /Volumes/puppet-agent-1.2.4-1.yosemite/puppet-agent-1.2.4-1.yosemite.pkg -target /
+sudo installer -pkg /Volumes/puppet-agent-1.2.4-1.yosemite/puppet-agent-1.2.4-1.yosemite.pkg -target /
 rm -f puppet-agent-1.2.4-1.yosemite.dmg
 sudo hdiutil unmount puppet-agent-1.2.4-1.yosemite.dmg
 
+export PATH=$PATH:/opt/puppetlabs/bin

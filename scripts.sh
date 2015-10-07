98b7324a1d45d55f0a60dea627e9470c5775d690
lucyhufton <lmh@amp.co>
2015-10-07 11:36:37 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-mv 50-xdebug.ini /Library/Server/Web/Config/php/50-xdebug.ini`
+mv 50-xdebug.ini /Library/Server/Web/Config/php/50-xdebug.ini
+sudo mkdir -p /Library/Server/Web/Config/php
+sudo ln -s /usr/local/etc/php/5.5/conf.d/ext-mcrypt.ini /Library/Server/Web/Config/php/ext-mcrypt.ini
 
 bash brew.sh
 bash cask.sh

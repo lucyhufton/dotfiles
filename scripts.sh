3593bfcbe6aabc5fc3ccbb5060261062815a7a8e
lucyhufton <lmh@amp.co>
2015-10-07 11:41:13 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -3,6 +3,15 @@
 mv 50-xdebug.ini /Library/Server/Web/Config/php/50-xdebug.ini
 sudo mkdir -p /Library/Server/Web/Config/php
 sudo ln -s /usr/local/etc/php/5.5/conf.d/ext-mcrypt.ini /Library/Server/Web/Config/php/ext-mcrypt.ini
+cat zend_extension=/usr/lib/php/extensions/no-debug-non-zts-20121212/xdebug.so >> /etc/php.ini.default
+sudo mv /etc/php.ini.default /etc/php.ini
+
+mkdir /ampersand/builds
+mkdir /root/ssl/devserver.crt
+mkdir /root/ssl/rapidssl.ca
+mkdir /root/ssl/devserver.key
+
+chmod 600 /root/ssl/devserver.key
 
 bash brew.sh
 bash cask.sh

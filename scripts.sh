ee5f5977d5b55f11a0fefef1c8f21dc2535c93fa
lucyhufton <lmh@amp.co>
2015-10-08 09:57:07 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-mv 50-xdebug.ini /Library/Server/Web/Config/php/50-xdebug.ini
-sudo mkdir -p /Library/Server/Web/Config/php
-sudo ln -s /usr/local/etc/php/5.5/conf.d/ext-mcrypt.ini /Library/Server/Web/Config/php/ext-mcrypt.ini
-cat zend_extension=/usr/lib/php/extensions/no-debug-non-zts-20121212/xdebug.so >> /etc/php.ini.default
-sudo mv /etc/php.ini.default /etc/php.ini
-
 mkdir /ampersand/builds
 mkdir /root/ssl/devserver.crt
 mkdir /root/ssl/rapidssl.ca
@@ -16,4 +10,20 @@ chmod 600 /root/ssl/devserver.key
 bash brew.sh
 bash cask.sh
 bash init.sh
+
+sudo mkdir -p /Library/Server/Web/Config/php
+sudo mv 50-xdebug.ini /Library/Server/Web/Config/php/50-xdebug.ini
+echo zend_extension=/usr/lib/php/extensions/no-debug-non-zts-20121212/xdebug.so >> /etc/php.ini.default
+sudo mv /etc/php.ini.default /etc/php.ini
+sudo mkdir /ampersand/builds
+find /ampersand -type d -exec chmod g+w {} \;
+
+sudo mkdir /root
+sudo mkdir /root/ssl
+touch /root/ssl/devserver.crt
+touch /root/ssl/devserver.key
+touch /root/ssl/rapidssl.ca
+chmod 600 /root/ssl/devserver.key
+
+sudo apachectl restart
 bash osx.sh
